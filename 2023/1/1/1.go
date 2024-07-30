08851c41da8a5ab6c2d89d2b4ead74c0d191b3df
Leonardo Gatti <leonardogattidev@gmail.com>
2024-07-30 14:34:03 -0300

    added 2023.1.1 solution

---
+++ 2023/1/1/1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	utils "go-stuff/aoc"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	input := utils.GetInput()
+
+	lines := strings.Split(input, "\n")
+
+	sum := 0
+
+	for _, line := range lines {
+		digits := getDigits(line)
+		fmt.Println(string(digits[:]))
+
+		num, _ := strconv.Atoi(string(digits[:]))
+		sum += num
+	}
+	fmt.Println(sum)
+}
+
+func getDigits(line string) [2]rune {
+	digits := [2]rune{}
+
+	for i := 0; i < len(line); i++ {
+		r := rune(line[i])
+		if unicode.IsDigit(r) {
+			digits[0] = r
+			break
+		}
+	}
+
+	for i := 0; i < len(line); i++ {
+		r := rune(line[len(line)-1-i])
+		if unicode.IsDigit(r) {
+			digits[1] = r
+			break
+		}
+	}
+
+	return digits
+}
